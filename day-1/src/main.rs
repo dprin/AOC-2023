f3f08150a374ca53e5f2908cfe23a3334e83f959
dprin <thenerfgamer09@gmail.com>
2023-12-15 23:20:46 +0100

    Test generation based on macro input

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -3,7 +3,7 @@ use day_macro::solution;
 const TEST_INPUT: &str = r"
 asdasd";
 
-#[solution(Year = 2023, Day = 1, Part = 1, Test = TEST_INPUT, Answer = 123)]
+#[solution(Year = 2023, Day = 1, Part = 1, Test = TEST_INPUT, Answer = 0)]
 fn something(data: &String) -> u32 {
-    unimplemented!()
+    0
 }
--- day-macro/src/lib.rs
+++ day-macro/src/lib.rs
@@ -1,74 +1,93 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse::Parse, Ident, Token, LitInt, ExprLit, parse_macro_input, ItemFn};
-use std::fmt::Display;
+use quote::{format_ident, quote, ToTokens};
+use std::fmt::Debug;
+use syn::Expr;
+use syn::{parse::Parse, parse_macro_input, Ident, LitInt, Token};
 
-const PARAMS: u32 = 5;
-
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct MacroInput {
     year: u32,
     day: u32,
     part: u32,
     test_input_name: Option<Ident>,
-    test_answer: Option<ExprLit>,
-}
-
-impl Display for MacroInput {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let inp = match &self.test_input_name {
-            Some(val) => format!("{}", val),
-            None => "None".to_string()
-        };
-
-        write!(f, "MacroInput {{ year: {}, day: {}, part: {}, test_input_name: {}, test_answer: {} }}", self.year, self.day, self.part, inp, "doesn't work :(")
-    }
+    test_answer: Option<Expr>,
 }
 
 impl Parse for MacroInput {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let mut ret = MacroInput::default();
 
-        for i in 0..PARAMS {
+        while !input.is_empty() {
             let var: Ident = input.parse()?;
             input.parse::<Token!(=)>()?;
 
             match var.to_string().as_str() {
-                "Year" => {ret.year = input.parse::<LitInt>()?.base10_parse::<u32>()?},
-                "Day" => {ret.day = input.parse::<LitInt>()?.base10_parse::<u32>()?},
-                "Part" => {ret.part = input.parse::<LitInt>()?.base10_parse::<u32>()?},
-                "Test" => {ret.test_input_name = Some(input.parse::<Ident>()?)},
-                "Answer" => {ret.test_answer = Some(input.parse::<ExprLit>()?)},
-                s => panic!("Invalid parameter: {}",  s),
+                "Year" => ret.year = input.parse::<LitInt>()?.base10_parse::<u32>()?,
+                "Day" => ret.day = input.parse::<LitInt>()?.base10_parse::<u32>()?,
+                "Part" => ret.part = input.parse::<LitInt>()?.base10_parse::<u32>()?,
+                "Test" => ret.test_input_name = Some(input.parse::<Ident>()?),
+                "Answer" => ret.test_answer = Some(input.parse::<Expr>()?),
+                s => panic!("Invalid parameter: {}", s),
             };
-            
-            if i != PARAMS - 1 {
+
+            if !input.is_empty() {
                 input.parse::<Token!(,)>()?;
             }
         }
 
-        if ret.test_answer.is_none() || ret.test_input_name.is_none() {
-            panic!("Please enter an test input and answer")
+        if ret.test_answer.is_none() ^ ret.test_input_name.is_none() {
+            panic!("Please enter either both test and answer or no test and no answer");
         }
 
         Ok(ret)
     }
 }
 
+fn generate_test(params: &MacroInput, fn_name: &Ident) -> TokenStream {
+    let test_name = format_ident!("test_part_{}", &params.part);
+
+    let input_name = params.test_input_name.clone().unwrap();
+
+    let answer = match params.test_answer.clone().unwrap() {
+        Expr::Lit(l) => l,
+        _ => panic!("Expected a literal for answer"),
+    };
+
+    let fn_input: Ident = format_ident!("{}", fn_name);
+
+    quote! {
+        #[test]
+        fn #test_name () {
+            assert_eq!(#fn_input(&String::from(#input_name)), #answer);
+        }
+    }
+    .into()
+}
+
+fn generate_main() -> TokenStream {
+    quote! {
+        fn main() {
+
+        }
+    }
+    .into()
+}
+
 #[proc_macro_attribute]
 pub fn solution(attr: TokenStream, item: TokenStream) -> TokenStream {
     let attr = parse_macro_input!(attr as MacroInput);
-    let item = parse_macro_input!(item as ItemFn);
-    let test_name: Ident = attr.test_input_name.clone().unwrap();
+    let item = parse_macro_input!(item as syn::ItemFn);
 
-    let main = quote! {
-        fn main() {
-            println!("Hello from macro!\n {}", #test_name);
-        }
-    };
-    
-    println!("{attr}");
-    println!("{:#?}", item);
+    let mut ret: TokenStream = TokenStream::new();
 
-    main.into()
+    // println!("{:#?}", attr);
+    // println!("{:#?}", item);
+
+    if attr.test_answer.is_some() {
+        ret.extend(&mut generate_test(&attr, &item.sig.ident).into_iter());
+    }
+    ret.extend(&mut generate_main().into_iter());
+    ret.extend(TokenStream::from(item.into_token_stream()).into_iter());
+
+    ret.into()
 }
