ca5064d5c6007e5b07d5bbf2ee20e944cf704d1c
dprin <thenerfgamer09@gmail.com>
2023-12-20 12:58:33 +0100

    Day 1 Part 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,17 +1,22 @@
 use day_macro::{solution, execute_day};
 
 #[allow(unused)]
-const TEST_INPUT: &str = r"
-asdasd";
+const PART_1: &str = r"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
 
-#[solution(2023, 1, 1, TEST_INPUT, 0)]
+#[solution(2023, 1, 1, PART_1, 142)]
 fn something(data: &String) -> u32 {
-    0
-}
+    data.lines()
+        .map(|line| {
+            let mut nums = line.chars().filter_map(|char| char.to_digit(10));
 
-#[solution(2023, 1, 2, TEST_INPUT, 0)]
-fn something_else(data: &String) -> u32 {
-    0
+            let first = nums.next().unwrap();
+            let last = nums.last().unwrap_or(first);
+            
+            (first * 10) + last
+        }).sum()
 }
 
 execute_day!(2023, 1);
